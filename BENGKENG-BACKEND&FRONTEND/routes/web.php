a68627275c0e8a2b0de9a4de429e98c62d89d7bb
rickosong <ricko.aprilianto00@gmail.com>
2022-09-28 23:50:19 +0800

    menambahkan Controller

--- BENGKENG-BACKEND&FRONTEND/routes/web.php
+++ BENGKENG-BACKEND&FRONTEND/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BengkelController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('user.landing');
-});
+// Route::get('/', function () {
+//     return view('user.landing');
+// });
+Route::get('/', [HomeController::class, 'index'])->name('landing');
