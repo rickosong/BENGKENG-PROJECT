e9efe9efaa8e217ba04486a2c46e505bd4496438
rickosong <ricko.aprilianto00@gmail.com>
2022-10-04 00:35:56 +0800

    make model

--- BENGKENG-BACKEND&FRONTEND/database/migrations/2022_10_03_162905_create_all_ratings_table.php
+++ BENGKENG-BACKEND&FRONTEND/database/migrations/2022_10_03_162905_create_all_ratings_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('all_ratings', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('all_ratings');
     }
 };
