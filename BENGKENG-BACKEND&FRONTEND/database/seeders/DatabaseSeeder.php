032e39b00b131582272dbe69f3638ba3f4a62308
rickosong <ricko.aprilianto00@gmail.com>
2022-10-09 23:05:05 +0800

    membuat seeder dan sedikit perubahan migrations

--- BENGKENG-BACKEND&FRONTEND/database/seeders/DatabaseSeeder.php
+++ BENGKENG-BACKEND&FRONTEND/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,13 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use App\Models\AllBengkel;
+use App\Models\AllRating;
+use App\Models\Bengkel;
+use App\Models\Review;
+use App\Models\Status;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +27,55 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Dummy User
+        User::create(
+            [
+                'email' => 'user1@gmail.com',
+                'password' => bcrypt('pass321'),
+                'phonenumber' => '081932432218',
+                'birth' => '04/04/2004',
+                'image' => 'user.svg',
+            ]
+        );
+
+        // Status Bengkel
+        Status::create(
+            [
+                'statusbengkel' => 'Aktif',
+            ],
+        );
+
+        Status::create(
+            [
+                'statusbengkel' => 'Tidak Aktif',
+            ]
+        );
+
+        // Jenis Rating
+        AllRating::create(
+            [
+                'rating' => 'Bagus'
+            ]
+        );
+
+        AllRating::create(
+            [
+                'rating' => 'Jelek'
+            ]
+        );
+
+        // Jenis Bengkel
+        AllBengkel::create(
+            [
+                'jenis_bengkel' => 'Bengkel Motor'
+            ]
+        );
+
+        AllBengkel::create(
+            [
+                'jenis_bengkel' => 'Bengkel Mobil'
+            ]
+        );
     }
 }
