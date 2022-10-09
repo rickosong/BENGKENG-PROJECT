705efb88861c1872e3151e58942df880c7b47223
rickosong <ricko.aprilianto00@gmail.com>
2022-10-09 18:38:28 +0800

    make new controller called ProfileController

--- BENGKENG-BACKEND&FRONTEND/app/Http/Controllers/ProfileController.php
+++ BENGKENG-BACKEND&FRONTEND/app/Http/Controllers/ProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class ProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,12 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('user.landing');
-    }
-
-    public function home()
-    {
-        return view('user.home');
+        //
     }
 
     /**
--- BENGKENG-BACKEND&FRONTEND/routes/web.php
+++ BENGKENG-BACKEND&FRONTEND/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BengkelController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
