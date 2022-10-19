512172db7a863e4f70b2869b328d6031366e2586
rickosong <ricko.aprilianto00@gmail.com>
2022-10-19 10:10:49 +0800

    ujicoba ngrok

--- BENGKENG-BACKEND&FRONTEND/resources/views/user/landing.blade.php
+++ BENGKENG-BACKEND&FRONTEND/resources/views/user/landing.blade.php
@@ -4,10 +4,10 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-		<link rel="stylesheet" href="../css/landing.css" />
-		<link rel="shortcut icon" href="../img/BENGKENG PROJECT.png" type="image/x-icon" />
+		<link rel="stylesheet" href="{{ asset('css/landing.css') }} " />
+		<link rel="shortcut icon" href="{{ asset('img/BENGKENG PROJECT.png') }}" type="image/x-icon" />
 		<title>BENGKENG | LANDING</title>
 	</head>
 	<body>
@@ -138,9 +138,9 @@
 	<!-- Footer End -->
 
 		<!-- jQuery -->
-		<script src="../js/jquery-3.6.0.min.js"></script>
+		<script src="{{ asset('../js/jquery-3.6.0.min.js') }}"></script>
 		<!-- <script src="../js/popper.min.js"></script> -->
-		<script src="../js/bootstrap.bundle.min.js"></script>
+		<script src="{{ asset('../js/bootstrap.bundle.min.js') }}"></script>
 
 		<!-- jQuery end -->
 	</body>
